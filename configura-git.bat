1e6626f05b816baf931e68e06013462dabd6ce80
LucasUesso <uessolucas47@gmail.com>
2024-08-09 21:42:10 -0300

    primeira pagina web

---
+++ configura-git.bat
@@ -0,0 +1,14 @@
+echo OFF
+echo --- Configurando Usuario GitHUB ----
+echo
+echo
+git config --global user.name "LucasUesso"
+git config --global user.email uessolucas47@gmail.com
+echo --- Mostrando a configuração atual do GitHUB no computador
+git config --list
+echo
+echo
+echo --- Fim ---
+echo Joao Lindo Saudades ---
+
+pause
